772949f28428e4bc499730d09a39b6b6b5423295
harun bozaci <harun.bozaci@hotmail.com>
2021-04-12 14:57:50 +0300

    yeni driver

--- new.c
+++ new.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 
+int gpio_driver_init(void);
+int gpio_driver_set(void);
+int gpio_driver_reset(void);
+int gpio_driver_close(void);
+
 int main(int argc,char** argv){
 	
 	printf("Hello Git!\n");
